0bcfb50b7e0483a9475e55f7088b818e9a55d03b
midiway <ramon@misoftware.rs>
2017-01-03 17:20:54 -0200

    NativeOxyPlot

--- NativeWebcam/Src/README.md
+++
@@ -1,5 +0,0 @@
-# Sciter Bootstrap for C&#x23;
-
-Visit http://misoftware.rs/Bootstrap for quickly creating a Sciter desktop app in C#.
-
-This repository holds the package for C# language.
--- NativeWebcam/Src/SciterBootstrap/scripts/postBuild.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if "%1"=="Debug" exit
-
-echo ######## Copying to Output ########
-cd %~dp0
-copy "..\bin\Release\*.dll" "..\..\..\"
-copy "..\res\*" "..\..\..\"
-del "..\..\..\sciter.dll"
